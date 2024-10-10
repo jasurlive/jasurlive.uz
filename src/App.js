d87de2ab2540279eaf35557455b26049ece3b226
jasurgraduate <jasur@graduate.org>
2024-10-11 01:15:15 +0900

    new features :)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';  // Main page component
+import Awards from './Awards';  // Awards page component
+import Portfolio from './Portfolio';  // Portfolio page component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/awards" element={<Awards />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Awards.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Awards() {
+    return (
+        <div>
+            <h1>Awards</h1>
+            <div className="container">
+                <img src="/img/ielts2019.png" alt="IELTS 2019" />
+                <img src="/img/ielts2022.png" alt="IELTS 2022" />
+                {/* Add more award images and content */}
+            </div>
+        </div>
+    );
+}
+
+export default Awards;
