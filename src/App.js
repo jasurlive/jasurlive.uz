14d9a3064337695320cf62e3c0c63c2fcc3f3ac5
jasurgraduate <jasur@graduate.org>
2024-11-29 16:30:16 +0900

    edited :)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './Home'; // Main page component
-import Resume from './Resume'; // Resume component
+import Home from './Home'; // Main page
+import Resume from './Resume';
 import Portfolio from './Portfolio';
 import Awards from './Awards';
 import Blogs from './Blogs';
+import Snowfall from "react-snowfall";
 
 function App() {
   return (
@@ -18,13 +19,27 @@ function App() {
         <Route path="/blogs" element={<Blogs />} />
       </Routes>
 
+      <Snowfall
+        color="white"       // Snowflake color
+        snowflakeCount={300} // Number of snowflakes
+        radius={[0, 6]}      // Radius (size range) of snowflakes (ensure this is an array)
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: 1000,        // Ensure snowfall stays behind content
+        }}
+      />
+
       {/* Navigation Menu */}
       <ul className="menu">
-        <li><Link to="/">&#127969; HOME</Link></li> {/* Internal route */}
-        <li><Link to="/resume">&#128373; RESUME</Link></li> {/* Internal route */}
-        <li><Link to="/portfolio">&#128092; PORTFOLIO</Link></li> {/* External link */}
-        <li><Link to="/awards">&#127891; AWARDS</Link></li> {/* Internal route */}
-        <li><Link to="/blogs">&#x1F334; BLOGS</Link></li> {/* External link */}
+        <li><Link to="/">&#127969; HOME</Link></li>
+        <li><Link to="/resume">&#128373; RESUME</Link></li>
+        <li><Link to="/portfolio">&#128092; PORTFOLIO</Link></li>
+        <li><Link to="/awards">&#127891; AWARDS</Link></li>
+        <li><Link to="/blogs">&#x1F334; BLOGS</Link></li>
       </ul>
     </Router>
   );
