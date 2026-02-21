40306d1f9e69a38a447d2fa54d5ab4ebdcd8faa2
jasurgraduate <jasur@graduate.org>
2026-02-21 16:41:34 +0500

    new spinner added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Online from "../add/tools/Online";
 import HomeSwiper from "../add/tools/SwiperHome";
 import Skills from "../add/tools/Skills";
@@ -8,24 +9,48 @@ import FramedMap from "../add/iframe/TravelMap";
 import DJ from "../add/iframe/DJ";
 import Speeches from "../pages/videos/Speeches";
 import Logo from "../add/tools/Logo";
-import "../add/css/home.css";
 import Social from "../add/tools/Social";
 import Code from "../add/tools/Code";
+import OrbitDotsLoader from "../add/tools/Spinner";
+import "../add/css/home.css";
 
 const Home: React.FC = () => {
-  return (
-    <div className="home-container">
-      <Logo />
-      <SMS />
-      <HomeSwiper />
-      <Speeches />
+  const [loading, setLoading] = useState(true);
 
-      <FramedMap />
-      <Projects />
-      <Online />
-      <Social />
-      <FullScreenToggle />
-    </div>
+  // Show loader until window fully loads
+  useEffect(() => {
+    const onLoad = () => setLoading(false);
+    if (document.readyState === "complete") {
+      onLoad();
+    } else {
+      window.addEventListener("load", onLoad);
+      return () => window.removeEventListener("load", onLoad);
+    }
+  }, []);
+
+  return (
+    <>
+      {loading && (
+        <div className="page-loader">
+          <OrbitDotsLoader />
+        </div>
+      )}
+
+      <div
+        className="home-container"
+        style={{ visibility: loading ? "hidden" : "visible" }}
+      >
+        <Logo />
+        <SMS />
+        <HomeSwiper />
+        <Speeches />
+        <FramedMap />
+        <Projects />
+        <Online />
+        <Social />
+        <FullScreenToggle />
+      </div>
+    </>
   );
 };
 
